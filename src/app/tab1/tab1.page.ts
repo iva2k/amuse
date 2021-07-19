84b8acb4b46a78fd203901f55b49d7b49037bf6f
Ilya <iva2k@yahoo.com>
2021-07-19 10:45:17 -0700

    Add ttm textarea

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  public textinput = [
+    'Scale C Major',
+    'Time 4/4',
+    'Tempo 76',
+    'Verse 4',
+    'I',
+    'V',
+    'vi',
+    'IV',
+    'Chorus 4',
+    'I',
+    'V',
+    'vi',
+    'IV',
+  ].join('\n');
   constructor() {}
+  onPlay() {
+    console.log('onPlay()');
+  }
 
 }
